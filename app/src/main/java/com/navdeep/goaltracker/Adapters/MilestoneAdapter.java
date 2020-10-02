d38a64fd24044f119c713b2db7cc70f7f293db09
niku1987 <navdeepst.1987@gmail.com>
2020-10-02 14:25:24 +1000

    code cleanup G02

--- app/src/main/java/com/navdeep/goaltracker/Adapters/MilestoneAdapter.java
+++ app/src/main/java/com/navdeep/goaltracker/Adapters/MilestoneAdapter.java
@@ -27,6 +27,9 @@ public class MilestoneAdapter extends RecyclerAdapter {
     private int milestoneCount;
     private Context context;
     private int goalId;
+    private TextView dayTitle, milestoneTitle, milestoneTimer, milestonetime, milestoneStatus;
+    private static String DEFAULT_TIME = "00:00:00";
+
     public MilestoneAdapter(ArrayList<Milestone> milestones, Context context, int goalId) {
         this.milestones = milestones;
         this.context = context;
@@ -44,48 +47,75 @@ public class MilestoneAdapter extends RecyclerAdapter {
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, final int position) {
         CardView cardView = holder.getCardView();
-        cardView.setCardBackgroundColor(context.getResources().getColor(R.color.baselineSurfaceColor));
-        TextView dayTitle = cardView.findViewById(R.id.dayTitle);
-        TextView milestoneTitle = cardView.findViewById(R.id.milestoneTitle);
-        TextView milestoneTimer = cardView.findViewById(R.id.milestoneTimer);
-        TextView milestonetime = cardView.findViewById(R.id.milestoneTime);
+        inflateCardView(cardView, position);
+        setOnClickListener(cardView, position);
+    }
 
-        TextView milestoneStatus = cardView.findViewById(R.id.task_status);
+    private void inflateCardView(CardView cardView, int position) {
+        findViewsOnCardView(cardView);
+        setViewsOnCardView(cardView, position);
+    }
+
+
+    private void findViewsOnCardView(CardView cardView) {
+        dayTitle = cardView.findViewById(R.id.dayTitle);
+        milestoneTitle = cardView.findViewById(R.id.milestoneTitle);
+        milestoneTimer = cardView.findViewById(R.id.milestoneTimer);
+        milestonetime = cardView.findViewById(R.id.milestoneTime);
+        milestoneStatus = cardView.findViewById(R.id.task_status);
+    }
+
+    private void setViewsOnCardView(CardView cardView, int position) {
+        cardView.setCardBackgroundColor(context.getResources().getColor(R.color.baselineSurfaceColor));
         dayTitle.setText(String.format("Day %d", (++milestoneCount)));
-        Calendar calendar = GoalUtil.getCalendarObject(milestones.get(position).getTime());
-        String title =  GoalUtil.dayOfWeek[calendar.get(Calendar.DAY_OF_WEEK)]+", "+calendar.get(Calendar.DATE)+" "
-                +GoalUtil.months[calendar.get(Calendar.MONTH)]+" "+calendar.get(Calendar.YEAR);
-        milestonetime.setText(String.format("%s",title));
+        milestonetime.setText(String.format("%s",createTitle(position)));
         milestoneTitle.setText(milestones.get(position).getTitle());
         milestoneTimer.setText(milestones.get(position).getTimer());
-        Log.i("MilestoneTimer", milestoneTimer.getText().toString());
-        if(!milestoneTimer.getText().toString().trim().equalsIgnoreCase("00:00:00")){
-
-         milestoneStatus.setBackgroundColor(context.getResources().getColor(R.color.baselineOkayColor));
-
-         milestoneStatus.setBackground(context.getResources().getDrawable(R.drawable.milestone_textview_active_border));
-         dayTitle.setBackgroundColor(context.getResources().getColor(R.color.baselineOkayColor));
-
+        if(!milestoneTimer.getText().toString().trim().equalsIgnoreCase(DEFAULT_TIME)){
+            setBackgroundToOkay();
         }else{
-            dayTitle.setBackgroundColor(context.getResources().getColor(R.color.baselineErrorColor));
-            milestoneStatus.setBackground(context.getResources().getDrawable(R.drawable.milestone_textview_idle_border));
+            setBackgroundToError();
         }
         milestoneStatus.setText(R.string.status_active);
         milestoneStatus.setTextColor(context.getResources().getColor(R.color.onBackgroundColor));
+    }
+
+    private String createTitle(int position) {
+        Calendar calendar = GoalUtil.getCalendarObject(milestones.get(position).getTime());
+        return  GoalUtil.dayOfWeek[calendar.get(Calendar.DAY_OF_WEEK)]+", "+calendar.get(Calendar.DATE)+" "
+                +GoalUtil.months[calendar.get(Calendar.MONTH)]+" "+calendar.get(Calendar.YEAR);
+    }
+
+    private void setBackgroundToOkay() {
+        milestoneStatus.setBackgroundColor(context.getResources().getColor(R.color.baselineOkayColor));
+        milestoneStatus.setBackground(context.getResources().getDrawable(R.drawable.milestone_textview_active_border));
+        dayTitle.setBackgroundColor(context.getResources().getColor(R.color.baselineOkayColor));
+    }
+
+    private void setBackgroundToError() {
+        dayTitle.setBackgroundColor(context.getResources().getColor(R.color.baselineErrorColor));
+        milestoneStatus.setBackground(context.getResources().getDrawable(R.drawable.milestone_textview_idle_border));
+    }
+
+    private void setOnClickListener(final CardView cardView, final int position) {
         cardView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(context, MilestoneInputActivity.class);
-                intent.putExtra(MilestoneInputActivity.MILESTONE_ID, milestonePresenter.getMilestones(goalId).get(position).getMilestoneId());
-                intent.putExtra(MilestoneInputActivity.GOAL_ID, goalId);
-                if(position==milestonePresenter.getMilestones(goalId).size()-1){
-                    intent.putExtra(MilestoneInputActivity.INPUT_FLAG , false);
-                }
-                context.startActivity(intent);
+                startMilestoneInputActivity(position);
             }
         });
-
     }
+
+    private void startMilestoneInputActivity(int position) {
+        Intent intent = new Intent(context, MilestoneInputActivity.class);
+        intent.putExtra(MilestoneInputActivity.MILESTONE_ID, milestonePresenter.getMilestones(goalId).get(position).getMilestoneId());
+        intent.putExtra(MilestoneInputActivity.GOAL_ID, goalId);
+        if(position==milestonePresenter.getMilestones(goalId).size()-1){
+            intent.putExtra(MilestoneInputActivity.INPUT_FLAG , false);
+        }
+        context.startActivity(intent);
+    }
+
     @Override
     public int getItemCount() {
         return milestones.size();
